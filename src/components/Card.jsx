bce3ba7fd7395b886cdd8e6ea04331a54865f18a
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2024-04-26 00:12:00 +1000

    Added Speaker function to the Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSpeechSynthesis } from "react-speech-kit";
 
-function Card() {
+function Card({ Card }) {
   const [isFlipped, setFlip] = useState(false);
+  const [question, setQuestion] = useState("Please Enter Question");
+  const [answer, setAnswer] = useState("Please Enter Answer");
+  const { speak, voices } = useSpeechSynthesis();
 
   const handleCardFlip = () => {
     setFlip(!isFlipped);
   };
+
+  const handleTextSpeak = (data) => {
+    // ADD SWITCH TO CONTROL SPEAK STOP, RESET, CONTINUE
+    // ADD ICON FOR RESET, STOP(REPLACE THE SPEAKER ICON)
+    const value = data.toLowerCase();
+    const voice = voices.find(
+      (data) => data.name === "Google UK English Female"
+    );
+    speak({
+      text: value,
+      voices: voice,
+    });
+  };
+
+  useEffect(() => {
+    if (!Card) return;
+    if (Card.question) setQuestion(Card.question);
+    if (Card.answer) setAnswer(Card.answer);
+    setFlip(false);
+  }, [Card]);
 
   return (
     <div className="card">
       <div
         className={`card__main card__main--front ${isFlipped ? "flipped" : ""}`}
       >
-        <div className="card__main--btn-speaker btn__main-speaker fa-solid fa-volume-high" />
+        <div
+          className="card__main--btn-speaker btn__main-speaker fa-solid fa-volume-high"
+          onClick={(e) => {
+            handleTextSpeak(question);
+          }}
+        />
         <div className="card__main--header">
           <div className="card__main--header-title">Question</div>
-          <div className="card__main--header--container-btn">
+          <div className="card__main--header-btn">
             <div className="card__main--header-btn btn__card--header fa-regular fa-pen-to-square" />
             <div className="card__main--header-btn btn__card--header fa-solid fa-trash" />
           </div>
         </div>
-        <div className="card__main--container-main">Test</div>
+        <div className="card__main--container-main">{question}</div>
         <div
           className="card__main--btn-flip btn btn__main-flip"
           onClick={handleCardFlip}
@@ -31,15 +60,20 @@ function Card() {
       <div
         className={`card__main card__main--back ${isFlipped ? "flipped" : ""}`}
       >
-        <div className="card__main--btn-speaker btn__main-speaker fa-solid fa-volume-high" />
+        <div
+          className="card__main--btn-speaker btn__main-speaker fa-solid fa-volume-high"
+          onClick={(e) => {
+            handleTextSpeak(answer);
+          }}
+        />
         <div className="card__main--header">
           <div className="card__main--header-title">Answer</div>
-          <div className="card__main--header--container-btn">
+          <div className="card__main--header-btn">
             <div className="card__main--header-btn btn__card--header fa-regular fa-pen-to-square" />
             <div className="card__main--header-btn btn__card--header fa-solid fa-trash" />
           </div>
         </div>
-        <div className="card__main--container-main">Back</div>
+        <div className="card__main--container-main">{answer}</div>
         <div
           className="card__main--btn-flip btn btn__main-flip"
           onClick={handleCardFlip}
--- src/pages/FlashCard.jsx
+++ src/pages/FlashCard.jsx
@@ -1,27 +1,38 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useReducer, useState } from "react";
 import Header from "../layouts/Header";
 import { useParams } from "react-router-dom";
 import { useSpeechSynthesis } from "react-speech-kit";
 import useFetch from "../functions/useFetch";
 import Card from "../components/Card";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "PREV_CARD":
+      if (state === 0) return state;
+      return state - 1;
+    case "NEXT_CARD":
+      return state + 1;
+    default:
+      return state;
+  }
+};
+
 function FlashCard() {
   const { id } = useParams();
   const [card, setCard] = useState({
     cards: [],
   });
-  const { speak } = useSpeechSynthesis();
+  const initialIndex = 0;
+  const [curIndex, handleCardMove] = useReducer(reducer, initialIndex);
 
   const { data, error, isLoading, refetch } = useFetch(
     `http://localhost:8000/api/cards/${id}`
   );
 
   useEffect(() => {
-    if (!data) return;
+    if (!data || !data.card) return;
     setCard(data.card);
   }, [data]);
-
-  // <button onClick={() => speak({ text: value })}>Speech</button>;
 
   return (
     <div className="main">
@@ -32,22 +43,32 @@ function FlashCard() {
         <div className="flashcard">
           <div className="flashcard__overview">
             <div className="flashcard__overview--total">
-              Flashcards-{card ? card.cards.length : "0"}
+              Flashcards-{card.cards ? card.cards.length : "0"}
             </div>
             <div className="flashcard__overview--btn btn btn--create-card">
               Create Card
             </div>
           </div>
           <div className="flashcard__content">
-            <div className="flashcard__btn flashcard__btn--prev btn__arr">
+            <div
+              className="flashcard__btn flashcard__btn--prev btn__arr"
+              onClick={(e) => handleCardMove({ type: "PREV_CARD" })}
+            >
               &larr;
             </div>
-            <Card />
-            <div className="flashcard__btn flashcard__btn--next btn__arr">
+            <Card Card={card.cards[curIndex]} />
+
+            <div
+              className="flashcard__btn flashcard__btn--next btn__arr"
+              onClick={(e) =>
+                card.cards.length - 1 > curIndex &&
+                handleCardMove({ type: "NEXT_CARD" })
+              }
+            >
               &rarr;
             </div>
           </div>
-          <div className="flashcard__count--cur">1 of 2</div>
+          <div className="flashcard__count--cur">{`${card.cards.length < 1 ? 0 : curIndex + 1} of ${card.cards.length}`}</div>
         </div>
       )}
     </div>
