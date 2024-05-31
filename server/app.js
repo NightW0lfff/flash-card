528b6cd7cc2211c1f66aa755a1700a69bb23cbc4
NightW0lfff <88699008+NightW0lfff@users.noreply.github.com>
2024-06-01 01:13:44 +1000

    Modified app server

--- server/app.js
+++ server/app.js
@@ -7,6 +7,13 @@ const itemRouter = require("./routes/itemRoutes");
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://nightw0lfff.github.io");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(express.json());
 
 app.use("/api/cards", cardRouter);
